ace195f7dd56c01ea4f3d63c33b59a243b1b05d1
mrchazaaa <charlie.howlett@live.co.uk>
2018-10-23 11:46:45 +0100

    decoded the message!

--- codeBreaker.py
+++ codeBreaker.py
@@ -1,30 +1,25 @@
 import binascii
 import re
 
-code = "A5 97 0E CD 3C F2 A0 F3 BF AF 24 18 FC E2 CB C9 06 63 BD 3A F2 CA B9 72 E5 CB 97 2E"
+code = "A5 97 0E CD 3C F2 A0 F3 BF AF 24 18 FC E2 CB C9 06 6E BD 3A F2 CA B9 72 E5 CB 97 2E"
 
 #array stores each hex seperated by space, after converting to binary string
 print(code.split())
+
+#convert each hex number to an 8 bit binary string and store in new array
 data = ['{0:08b}'.format(int(i, 16)) for i in code.split()]
 
-print(data)
-
-#add all binary strings together (removing '0b from front')
+#concatenate all binary strings together
 stringData = ''
 for i in range(0, len(data)):
-    stringData += str(data[i])[2:]
+    stringData += str(data[i])
 
-#split into a list of strings of length 7
-# newData = re.findall(r'.{,6}''.',stringData) 
-newData = [stringData[i:i+7] for i in range(0, len(stringData), 7)]
+#split long into a list of smaller binary strings of length 7 bits
+newData = re.findall(r'.{,6}''.',stringData) 
 
-
-# for i in newData:
-#     print(int(i,2))
-
-#convert and display
+#convert each 7 bit string to ascii and display and display
 myString = ""
-for i in newData:
-    myString += chr(int(i, 2));
+for i in range(0, len(newData)):
+    myString += chr(int(newData[i], 2)) 
 
 print(myString)
